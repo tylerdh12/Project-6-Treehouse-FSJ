a55faaeebc902c3bdd32ba1aa88380b0573f6c89
Tyler Harper <tylerdh12@gmail.com>
2020-02-21 14:55:12 -0800

    Updated Portfolio

--- routes/index.js
+++ routes/index.js
@@ -1,41 +1,56 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { data } = require('../data.json');
+const { data } = require("../data.json");
 const { projects } = data;
 
-router.get('/', (req, res) => {
-    const { id } = req.params;
+router.get("/", (req, res) => {
+  const { id } = req.params;
 
-    res.render('index', { 
-        projects: projects,
-        name: data.name,
-        avatar: data.avatar,
-        projectName: data.project_name,
-        about: data.about,
-        id: projects.id,
-        project_name: projects.project_name, 
-        description: projects.description, 
-        technologies: projects.technologies, 
-        live_link: projects.live_link,
-        github_link: projects.github_link, 
-        image_urls: projects.image_urls, 
-        status: projects.status
-    });
+  res.render("index", {
+    projects: projects,
+    name: data.name,
+    avatar: data.avatar,
+    projectName: data.project_name,
+    about: data.about,
+    id: projects.id,
+    project_name: projects.project_name,
+    description: projects.description,
+    technologies: projects.technologies,
+    live_link: projects.live_link,
+    github_link: projects.github_link,
+    image_urls: projects.image_urls,
+    status: projects.status
+  });
 });
 
-router.get('/about', (req, res) => {
-    res.render('about', {
-        name: data.name,
-        avatar: data.avatar,
-        about: data.about,
-        bio: data.bio,
-        skills: data.skills,
-        phone: data.phone,
-        email: data.email,
-        linkedin: data.linkedin_account,
-        github: data.github_account,
-        twitter: data.twitter_account,
-    });
+router.get("/about", (req, res) => {
+  res.render("about", {
+    name: data.name,
+    avatar: data.avatar,
+    about: data.about,
+    bio: data.bio,
+    skills: data.skills,
+    phone: data.phone,
+    email: data.email,
+    linkedin: data.linkedin_account,
+    github: data.github_account,
+    twitter: data.twitter_account
+  });
+});
+
+router.get("/contact", (req, res) => {
+  res.render("contact", {
+    name: data.name,
+    avatar: data.avatar,
+    about: data.about,
+    bio: data.bio,
+    skills: data.skills,
+    phone: data.phone,
+    email: data.email,
+    linkedin: data.linkedin_account,
+    github: data.github_account,
+    twitter: data.twitter_account
+  });
 });
 
 module.exports = router;
